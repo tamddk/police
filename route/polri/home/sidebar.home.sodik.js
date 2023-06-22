ec78796adb346bb2a0456d6bc8c8493c6a91e032
tamddk <siapaa.akuhh@gmail.com>
2023-06-22 15:43:50 +0700

    New Update

--- route/polri/home/sidebar.home.sodik.js
+++ route/polri/home/sidebar.home.sodik.js
@@ -19,7 +19,7 @@ export function sidebar_home() {
             alt="${page_title_police}<"
           />
           <span class="t">Text Icon</span>
-          <span class="dark:text-gray-400 text-xl px-4 mt-2 font-bold">${page_title_police}<</span>
+          <span class="dark:text-gray-400 text-xl px-4 mt-2 font-bold">${page_title_police}</span>
         </div>
         <nav class="ls uh ym">
           <ul role="list" class="ls uh ym aaq">
